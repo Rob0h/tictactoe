f075d850736004f7e04b56d6ff0e911f29dd40d9
Rob0h <robert.is.oh@gmail.com>
2017-01-23 15:11:33 -0800

    Implement command-line prompt of positioning and player

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var inquirer = require('inquirer');
+
 // generate board
 function TicTacToe () {
   this.board = [['', '', ''], ['', '', ''], ['', '', '']];
@@ -19,14 +21,51 @@ TicTacToe.prototype.check =  function() {
   }
 }
 
-var test = new TicTacToe();
-test.place(0, 0, 'X');
-console.log(test.board);
+// questions for player input
+var questions = [
+  {
+    name: 'player',
+    type: 'input',
+    message: 'Enter your player: X or O',
+    validate: function( value ) {
+      if (value === 'X' || value === 'O') {
+        return true;
+      } else {
+        return 'Please enter either X or O';
+      }
+    }
+  },
+  {
+    name: 'Xposition',
+    type: 'input',
+    message: 'Enter the desired X position:',
+    validate: function(position) {
+      // Enter position validation
+      return true;
+    }
+  },
+  {
+    name: 'Yposition',
+    type: 'input',
+    message: 'Enter the desired Y position:',
+    validate: function(position) {
+      // Enter position validation
+      return true;
+    }
+  }
+];
 
+// initializes game
 function playGame() {
-  var test = new TicTacToe();
-  console.log(test);
-  var res = window.prompt('hi');
+  var game = new TicTacToe();
+  console.log(game.board);
+  while(!game.check) {
+    inquirer.prompt(questions).then(function (answers) {
+      console.log(answers);
+      game.place(answers.Xposition, answers.Yposition, answers.player);
+      console.log(game);
+    });
+  }
 }
 
 playGame();
